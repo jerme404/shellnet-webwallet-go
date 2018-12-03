982fd1c5f84e0da1f62e253bc6447a0e9be5a9d2
jerme404 <29779465+jerme404@users.noreply.github.com>
2018-12-02 21:01:46 -0500

    Update transaction_db.sql

--- transaction_db.sql
+++ transaction_db.sql
@@ -10,6 +10,6 @@ CREATE TABLE transactions (
 ID serial NOT NULL PRIMARY KEY,
 addr_id serial references addresses(id),
 DEST char(101),
-AMOUNT numeric(15,2) NOT NULL,
+AMOUNT numeric(15,9) NOT NULL,
 hash char(64) NOT NULL,
 paymentID char(64) not null);
