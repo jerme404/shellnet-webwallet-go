0edb56825338490fa0c576fb32ff290639fe67cc
JerMe404 <29779465+jerme404@users.noreply.github.com>
2019-02-04 12:29:25 -0500

    ac changes

--- services/wallet/wallet.go
+++ services/wallet/wallet.go
@@ -25,8 +25,8 @@ var (
 )
 
 // Forking config.
-var addressFormat = "^TRTL([a-zA-Z0-9]{95}|[a-zA-Z0-9]{183})$"
-var divisor float64 = 100 // This is 100 for TRTL
+var addressFormat = "^amit([a-zA-Z0-9]{94}|[a-zA-Z0-9]{182})$"
+var divisor float64 = 10000 // This is 100 for TRTL
 var transactionFee = 10 // This is 10 for TRTL
 
 func init() {
